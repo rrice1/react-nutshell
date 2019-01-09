958b98a3a4826d336ba08d043988bb591e16ca60
Robert Rice <rrice1291@gmail.com>
2019-01-08 21:25:10 -0600

    added stuff

---
+++ src/components/pages/Articles/Articles.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Articles.scss';
+
+class Articles extends React.Component {
+  render() {
+    return (
+      <div>
+      <h2>Articles Component</h2>
+      </div>
+    );
+  }
+}
+
+export default Articles;
---
+++ src/components/pages/Events/Events.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Events.scss';
+
+class Events extends React.Component {
+  render() {
+    return (
+      <div>
+      <h2>Events Component</h2>
+      </div>
+    );
+  }
+}
+
+export default Events;
