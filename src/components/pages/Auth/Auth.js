441cf3c51105b927e3b3843ea96ec745dc28e477
Robert Rice <rrice1291@gmail.com>
2019-01-08 19:11:32 -0600

    routing fun

--- src/components/pages/Auth/Auth.js
+++ src/components/pages/Auth/Auth.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import authRequests from '../../Helpers/data/authRequests';
+import authRequests from '../../../Helpers/data/authRequests';
 import './Auth.scss';
 
 import googleButton from './images/googlebutton.png';
 
 class Auth extends React.Component {
-  static propTypes = {
-    isAuthenticated: PropTypes.func,
-  }
-
   authenticateUser = (e) => {
     e.preventDefault();
     authRequests.authenticate().then(() => {
-      this.props.isAuthenticated();
+      this.props.history.push('/home');
     }).catch(err => console.error('there was an error with auth', err));
   }
 
