04d9cf14fb0a5c94d2dbc1176e7c6097fb725715
Robert Rice <rrice1291@gmail.com>
2019-01-15 19:58:52 -0600

    added friend

--- src/components/pages/Friends/FriendItems/FriendItems.js
+++ src/components/pages/Friends/FriendItems/FriendItems.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Button } from 'reactstrap';
 import friendShape from '../../../../Helpers/props/friendShape';
+import authRequests from '../../../../Helpers/data/authRequests';
 import './FriendItems.scss';
 
 class FriendItem extends React.Component {
@@ -9,6 +10,7 @@ class FriendItem extends React.Component {
     friend: friendShape,
     status: PropTypes.string,
     endFriendship: PropTypes.func,
+    addFriend: PropTypes.func,
   };
 
   deleteEvent = (e) => {
@@ -18,15 +20,51 @@ class FriendItem extends React.Component {
     endFriendship(friendRequestId);
   }
 
+  addFriendEvent = (e) => {
+    e.preventDefault();
+    const uid = authRequests.getCurrentUid();
+    const friendUid = e.target.closest('button').id;
+    const { addFriend } = this.props;
+    const newFriend = {
+      friendUid,
+      isAccepted: false,
+      isPending: true,
+      uid,
+    };
+    addFriend(newFriend);
+  }
+
   render() {
-    const { friend } = this.props;
+    const { friend, status } = this.props;
+    const makeButtons = () => {
+      if (status === 'confirmed') {
+        return (
+          <Button color="danger" id={friend.friendRequestId} onClick={this.deleteEvent}><i className="far fa-trash-alt"></i></Button>
+        );
+      } if (status === 'potentials') {
+        return (
+          <Button color="success" id={friend.uid} onClick={this.addFriendEvent}><i className="fas fa-plus"></i></Button>
+        );
+      } if (status === 'pending' && friend.friendRequest === 'them') {
+        return (
+          <div>
+            <Button color="success" id={friend.friendRequestId} onClick={this.acceptEvent}><i className="fas fa-user-check"></i></Button>
+            <Button color="danger" id={friend.friendRequestId} onClick={this.deleteEvent}><i className="far fa-trash-alt"></i></Button>
+          </div>
+        );
+      } return (
+        <div>
+          <p>pending</p>
+        </div>
+      );
+    };
 
     return (
-        <li className="friend-item" id={friend.id}>
-          <span className="col-1"><img className="photo" src={friend.photo} alt={friend.userName}/></span>
-          <span className="col-9">{friend.userName}</span>
-          <Button color="danger" id={friend.friendRequestId} onClick={this.deleteEvent}><i className="far fa-trash-alt"></i></Button>
-        </li>
+      <li className="friend-item" id={friend.id}>
+        <span className="col-1"><img className="photo" src={friend.photo} alt={friend.userName}/></span>
+        <span className="col-9">{friend.userName}</span>
+        <div>{makeButtons()}</div>
+      </li>
     );
   }
 }
--- src/components/pages/Friends/Friends.js
+++ src/components/pages/Friends/Friends.js
@@ -6,83 +6,100 @@ import FriendItem from './FriendItems/FriendItems';
 import friendRequests from '../../../Helpers/data/friendRequests';
 
 class Friends extends React.Component {
-state = {
-  confirmed: [],
-  pending: [],
-  potentials: [],
-}
+  state = {
+    potentials: [],
+    confirmed: [],
+    pending: [],
+  }
 
-componentDidMount() { // behind the scenes
-  this.getAndSortUsers();
-}
+  componentDidMount() {
+    this.getAndSortUsers();
+  }
 
-getAndSortUsers = () => {
-  const uid = authRequests.getCurrentUid();
-  smashRequests
-    .usersAndFriends(uid)
-    .then((results) => {
-      const users = results;
-      const potentials = users.filter(user => !user.isAccepted && !user.isPending);
-      const pending = users.filter(user => !user.isAccepted && user.isPending);
-      const confirmed = users.filter(user => user.isAccepted);
-      this.setState({
-        users,
-        potentials,
-        pending,
-        confirmed,
-      });
-    })
-    .catch(err => console.error('error in SMASH', err));
-}
+  getAndSortUsers = () => {
+    const uid = authRequests.getCurrentUid();
+    smashRequests
+      .usersAndFriends(uid)
+      .then((results) => {
+        const potentials = results.filter(user => !user.isAccepted && !user.isPending);
+        const pending = results.filter(user => !user.isAccepted && user.isPending);
+        const confirmed = results.filter(user => user.isAccepted);
+        this.setState({
+          potentials,
+          pending,
+          confirmed,
+        });
+      })
+      .catch(err => console.error('error in SMASH', err));
+  }
 
-endFriendship = (friendRequestId) => {
-  friendRequests.deleteFriend(friendRequestId)
-    .then(() => {
-      this.getAndSortUsers();
-    })
-    .catch(err => console.error('error in ending friendship', err));
-}
+  endFriendship = (friendRequestId) => {
+    friendRequests.deleteFriend(friendRequestId)
+      .then(() => {
+        this.getAndSortUsers();
+      })
+      .catch(err => console.error('error in ending friendship', err));
+  }
 
-render() {
-  const {
-    potentials,
-    pending,
-    confirmed,
-  } = this.state;
+  addFriend = (newFriend) => {
+    friendRequests.addFriend(newFriend)
+      .then(() => {
+        this.getAndSortUsers();
+      })
+      .catch(err => console.error('error in adding friend', err));
+  }
 
-  const friendItemComponents = (friendArray, status) => (
-    friendArray.map(friend => (
-      <FriendItem
-        key={friend.id}
-        friend={friend}
-        status={status}
-        endFriendship={this.endFriendship}
-      />
-    ))
-  );
+  confirmFriend = (friendId) => {
+    friendRequests.acceptFriendship(friendId)
+      .then(() => {
+        this.getAndSortUsers();
+      })
+      .catch(err => console.error('error in confirming friendship', err));
+  }
 
-  return (
-    <div className='Friends container'>
-      <h2>Friends</h2>
-      <div className="container">
-        <div className="row">
-          <div className="col-sm">
-            <h3>Potential Friends</h3>
-            <ul>{friendItemComponents(potentials)}</ul>
-          </div>
-          <div className="col-sm">
-            <h3>Pending Requests</h3>
-            <ul>{friendItemComponents(pending)}</ul>
-          </div>
-          <div className="col-sm">
-            <h3>Friends</h3>
-            <ul>{friendItemComponents(confirmed)}</ul>
+  render() {
+    const {
+      potentials,
+      pending,
+      confirmed,
+    } = this.state;
+
+    const friendItemComponents = (friendArray, status) => (
+      friendArray.map(friend => (
+        <FriendItem
+          key={friend.id}
+          friend={friend}
+          status={status}
+          endFriendship={this.endFriendship}
+          addFriend={this.addFriend}
+          confirmFriend={this.confirmFriend}
+        />
+      ))
+    );
+
+    return (
+      <div className='Friends'>
+        <h2>Friends</h2>
+        <div className="container">
+          <div className="row">
+            <div className="col-sm">
+              <h3>Potential Friends</h3>
+              <ul>{friendItemComponents(potentials, 'potentials')}</ul>
+            </div>
+            <div className="col-sm">
+              <h3>Pending Requests</h3>
+              <ul>{friendItemComponents(pending, 'pending')}</ul>
+            </div>
+            <div className="col-sm">
+              <h3>Friends</h3>
+              <ul>{friendItemComponents(confirmed, 'confirmed')}</ul>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-}
+
 
 export default Friends;
