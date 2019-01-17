acaa628b9a63e950c6054617ef1e17be10547a8a
Robert Rice <rrice1291@gmail.com>
2019-01-16 19:07:28 -0600

    reimported friendRequests and reseeded data

--- src/Helpers/data/authRequests.js
+++ src/Helpers/data/authRequests.js
@@ -10,8 +10,11 @@ const logoutUser = () => firebase.auth().signOut();
 
 const getCurrentUid = () => firebase.auth().currentUser.uid;
 
+const getCurrentUserName = () => firebase.auth().currentUser.displayName;
+
 export default {
   authenticate,
   logoutUser,
   getCurrentUid,
+  getCurrentUserName,
 };
